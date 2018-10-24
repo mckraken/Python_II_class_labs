62ba09abc5182cd3a85352d7cb5ef31b67a7e173
Stephen McCracken <mckraken@mckraken.net>
2018-10-24 13:03:13 -0500

    lab 5b

---
+++ lab05b.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+"""Temperature Conversion
+
+This program converts one temperature from fahrenheit to centigrade
+(in a void or fruitless function) and prints the results.
+Change it to handle a variable number of temperatures to covert and
+print in the function.
+"""
+
+from sys import argv, stderr
+
+
+def fahrenheit_to_centigrade(*args):
+    # for fdeg in [float(a) for a in args if str(a).isnumeric()]:
+    for item in args:
+        try:
+            fdeg = float(item)
+        except ValueError:
+            print(f'{item} ({type(item)}) is not valid for conversion', file=stderr)
+            continue
+
+        cdeg = 5 / 9 * (float(fdeg) - 32)
+        print(f'{float(fdeg):>7.1f} degrees Fahrenheit is {cdeg:>7.1f} degrees Centigrade')
+
+
+fahrenheit_to_centigrade(*argv[1:])
